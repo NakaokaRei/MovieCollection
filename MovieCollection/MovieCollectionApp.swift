f709ebaf747bf221b87844818c236317356b4497
NakaokaRei <reideeplearning@gmail.com>
2023-05-05 16:59:29 +0900

    Initial Commit

---
+++ MovieCollection/MovieCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionApp.swift
+//  MovieCollection
+//
+//  Created by NakaokaRei on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct MovieCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
